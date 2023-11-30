60cc801368029b50e097b48fe586ddf493b4b56c
Bipin_mango <bipin@mangosoftsolution.com>
2023-11-30 17:09:25 +0545

    css

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,41 +10,44 @@
     <link rel="stylesheet" href="/css/home.css">
 </head>
 <body>
-    <header>
-        <div class="login">
-            <ul class="links">
-                <a href="#" class="icon"><i class='bx bxl-facebook-circle'></i></a>
-                <a href="#" class="icon"><i class='bx bxl-linkedin'></i></a>
-                <form action="#" method="GET">
-                    <div class="searchBox">
+    <nav class="navbar_first flex">
+        <div class="left flex">
+            <a href="#" class="icon"><i class='bx bxl-facebook-circle'></i></a>
+            <a href="#" class="icon"><i class='bx bxl-linkedin'></i></a>
+            <form action="#" method="GET">
+                <div class="searchBox">
 
-                        <input class="searchInput" type="text" name="" placeholder="Search something">
-                        <button class="searchButton" href="#">
-                            <i class='bx bx-search'></i>
-                        </button>
-                    </div>
-                </form>
-                    <li><a href="{{ route('login') }}">login</a></li>
-                    <li><a href="{{ route('registration') }}">Signup</a></li>
-                    <li><a href="#">Contact Us </a></li>
-                    <li><a href="#"> Write for us</a></li>
+                    <input class="searchInput" type="text" name="" placeholder="Search something">
+                    <button class="searchButton" href="#">
+                        <i class='bx bx-search'></i>
+                    </button>
+                </div>
+            </form>
+        </div>
+        <div class="right flex">
+            <ul class="flex">
+                <li><a href="{{ route('login') }}">login</a></li>
+                <li><a href="{{ route('registration') }}">Signup</a></li>
+                {{-- <li><a href="#">Contact Us </a></li>
+                <li><a href="#"> Write for us</a></li> --}}
             </ul>
         </div>
-    </header>
-    <nav>
-        <div class="navigation">
-            <div class="logo"><a href="/">NEWS</a></div>
-            <ul class="links">
+    </nav>
+    <nav class="navbar_second flex">
+            <div class="logo"><h1>NEWS</h1></div>
+            <ul class="flex">
                 <li><a href="/">Home</a></li>
                 <li><a href="#">News</a></li>
                 <li><a href="#">Economy</a></li>
                 <li><a href="#">Market</a></li>
                 <li><a href="#">Company</a></li>
             </ul>
-        </div>
     </nav>
+    <section>
+        <main>
+            @yield('content')
+        </main>
+    </section>
 </body>
 </html>
-<main>
-    @yield('content')
-</main>
+
